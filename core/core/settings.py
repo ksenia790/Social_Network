ce7edc2bf523382147e750db3901feb2d67a7ef8
KseniyaSaltik <irbis.mr@gmail.com>
2022-06-01 21:53:05 +0300

    Update readme

--- core/core/settings.py
+++ core/core/settings.py
@@ -94,7 +94,7 @@ DATABASES = {
         'NAME': 'networkdb', 
         'USER': 'postgres', 
         'PASSWORD': 'sena',
-        'HOST': '127.0.0.1', 
+        'HOST': 'db', 
         'PORT': '5432',
     }
 }
